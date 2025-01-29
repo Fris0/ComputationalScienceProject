1fd2f475a04dd1295c8704afa7a4825224d3d987
HansvanBreda <hans.van.breda@student.uva.nl>
2025-01-29 14:21:45 +0100

    Update fps_to_cd.py

--- fps_to_cd.py
+++ fps_to_cd.py
@@ -8,7 +8,7 @@ import scipy
 import scipy.interpolate
 
 
-Machs = [
+Machs1 = [
     0.00,
     0.38,
     0.46,
@@ -44,7 +44,7 @@ Machs = [
     30.00
 ]
 
-Cds = [
+Cds_nike = [
     0.47,
     0.59,
     0.63,
@@ -80,20 +80,86 @@ Cds = [
     0.48,
 ]
 
-feet_per_secs = [1125.33 * mach for mach in Machs]
+Machs2 = [
+    1.00,
+    1.25,
+    1.50,
+    1.75,
+    2.00,
+    2.50,
+    3.00,
+    3.50,
+    4.00,
+    4.50,
+    5.00,
+    5.50,
+    6.00,
+    6.50,
+    7.00,
+    7.50,
+    8.00,
+    30.00
+]
+
+Cds_apache_coasting = [
+    0.930,
+    0.841,
+    0.785,
+    0.740,
+    0.704,
+    0.643,
+    0.590,
+    0.544,
+    0.507,
+    0.479,
+    0.454,
+    0.432,
+    0.412,
+    0.396,
+    0.388,
+    0.384,
+    0.384,
+    0.384
+]
+
+Cds_apache_thrusting = [
+    0.780,
+    0.707,
+    0.665,
+    0.634,
+    0.607,
+    0.564,
+    0.527,
+    0.496,
+    0.467,
+    0.444,
+    0.423,
+    0.402,
+    0.384,
+    0.374,
+    0.368,
+    0.364,
+    0.364,
+    0.364
+]
+
+feet_per_secs1 = [1125.33 * mach for mach in Machs1]
+feet_per_secs2 = [1125.33 * mach for mach in Machs2]
 
 
-def create_f():
-    xs = np.array(feet_per_secs)
+def create_f(fps, Cds):
+    xs = np.array(fps)
     ys = np.array(Cds)
     linear_function = scipy.interpolate.interp1d(xs, ys, kind='linear')
     return linear_function
 
 
-fps_to_Cd = create_f()
+Cd_nike_thrusting = create_f(feet_per_secs1, Cds_nike)
+Cd_apache_coasting = create_f(feet_per_secs2, Cds_apache_coasting)
+Cd_apache_thrusting = create_f(feet_per_secs2, Cds_apache_thrusting)
 
 if __name__ == "__main__":
-    plt.scatter(feet_per_secs[:], Cds[:], label="data")
+    plt.scatter(feet_per_secs1[:], Cds_nike[:], label="data")
     t = np.linspace(0, 11000, 3500)
     plt.plot(t, fps_to_Cd(t), color="red", label="interpolated")
     plt.xlabel("feet per second")
