152852018dd5a9c48bd74ef4a83a26b6d4c66b13
Mark Jansen <markjansen@sp-byods-145-109-55-29.wireless.uva.nl>
2025-01-29 14:56:08 +0100

    Plotting for thrust

--- main.py
+++ main.py
@@ -57,13 +57,13 @@ def plot_while_processing():
     ax[0].legend()
     plt.show()
 
-def obtain_data_from_simulation():
+def obtain_angle_data_from_simulation():
     la = np.linspace(0, 90, 10)  # Testing angles.
 
     with open("data.txt", "w") as f:
         for angle in la:
             # Resetting / Starting simulation.
-            solver = Solver(tend=400)
+            solver = Solver(tend=1000)
 
             # Simulating with new angle.
             result = solver.solve_rocket2d(Rocket(la=angle))
@@ -75,16 +75,64 @@ def obtain_data_from_simulation():
             # Writing to file
             f.write(f"{angle} {np.max(distance)} {np.max(altitude)}\n")
 
-def read_data_and_plot():
-    with open("data.txt", "r") as f:
-        while True:
-            line = f.readline()
-            if line != "":
-                res = np.array(list(map(float, line.split(sep=" "))))
-                print(res)
-                plt.bar(res[0], res[1])
-            else:
-                break
+def obtain_thrust_data_from_simulation():
+    thrust_samples = np.random.normal(5130, 0.05*5130, 1000)
+    with open("data.txt", "w") as f:
+        for idx, thrust in enumerate(thrust_samples):
+            # Resetting / Starting simulation.
+            solver = Solver(tend=1000)
+
+            # Simulating with new angle.
+            result = solver.solve_rocketNike(Rocket(T= thrust * 32.174))
+
+            # Extracting data.
+            distance = np.nan_to_num(result[0][:, 0].reshape(1, -1)[0], 0)
+            altitude = np.nan_to_num(result[0][:, 1].reshape(1, -1)[0], 0)
+            velocity = np.nan_to_num(result[0][:, 2].reshape(1, -1)[0], 0)
+            mass = np.nan_to_num(result[0][:, 3].reshape(1, -1)[0], 0)
+            time = np.linspace(solver.tbegin, solver.tend, len(distance))
+
+            # Writing to file
+            d = {'run': idx, 'distance': distance, 'altitude': altitude, 'velocity': velocity, 'mass': mass, 'time': time}
+            data = pd.DataFrame(data=d)
+            data.to_csv('thrust_data.csv', mode='a', index=False)
+
+def plot_thrust_data_from_simulation():
+    df = pd.read_csv("thrust_data.csv", low_memory=False)
+    df["run"] = pd.to_numeric(df["run"], errors="coerce")
+
+    start = df.run.min()
+    end = df.run.max()
+
+    fig, ax = plt.subplots(1,4, figsize=(16,8))
+
+    for run in range(int(start), int(end) + 1):
+
+        df_run = df[df["run"] == run]
+
+        # Extracting data.
+        distance = df_run["distance"].to_numpy(dtype=np.float64)
+        altitude = df_run["altitude"].to_numpy(dtype=np.float64)
+        velocity = df_run["velocity"].to_numpy(dtype=np.float64)
+        mass = df_run["mass"].to_numpy(dtype=np.float64)
+
+        # Plotting the different angles for analysis
+        ax[0].plot(distance, altitude)
+        ax[0].set_xlabel("Distance (ft)")
+        ax[0].set_ylabel("Altitude (ft)")
+        ax[1].plot(np.linspace(0, 1000, len(mass)), mass)
+        ax[1].set_xlabel("Time (s)")
+        ax[1].set_ylabel("Mass (lbs)")
+        ax[2].plot(np.linspace(0, 1000, len(mass)), velocity)
+        ax[2].set_xlabel("Time (s)")
+        ax[2].set_ylabel("Velocity (ft/s)")
+        ax[3].plot(velocity, altitude)
+        ax[3].set_xlabel("Velocity (ft/s)")
+        ax[3].set_ylabel("Altitude (ft)")
+
+    ax[0].set_ylim([-1, None])
+    ax[0].set_xlim([-1, None])
+    ax[0].legend()
     plt.show()
 
 if __name__ == "__main__":
@@ -97,6 +145,8 @@ if __name__ == "__main__":
     if option == 1:
         plot_while_processing()
     elif option == 2:
-        obtain_data_from_simulation()
+        obtain_angle_data_from_simulation()
+    elif option == 3:
+        obtain_thrust_data_from_simulation()
     else:
-        read_data_and_plot()
+        plot_thrust_data_from_simulation()
